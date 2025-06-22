d0bc5442ac607c1317ea6ea0d9e643d8d20e3076
isd576 <fizzyizzy0205@gmail.com>
2025-06-22 15:46:05 -0400

    it can search!

--- libby.py
+++ libby.py
@@ -3,7 +3,9 @@ import warnings
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
 warnings.filterwarnings("ignore")
 
 options=webdriver.FirefoxOptions()
@@ -12,3 +14,14 @@ service = Service(r"C:\Users\fizzy\Downloads\geckodriver-v0.36.0-win64\geckodriv
 driver = webdriver.Firefox(service=service, options=options)
 
 driver.get("https://libbyapp.com/search/nh")
+
+
+
+try:
+    search_input = WebDriverWait(driver, 15). until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[type=search]")))
+    search_input.click()
+    search_input.send_keys("Little Women")
+    search_input.send_keys(Keys.RETURN)
+except Exception as e:
+    print("Search bar not found", e)
+    driver.quit()
