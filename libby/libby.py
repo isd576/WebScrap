ba574ea781ae85aa3b49d4a8804d811baf0d8176
isd576 <fizzyizzy0205@gmail.com>
2025-07-29 11:41:55 -0400

    able to identify first availble book

--- libby/libby.py
+++ libby/libby.py
@@ -11,17 +11,28 @@ warnings.filterwarnings("ignore")
 
 options=webdriver.FirefoxOptions()
 service = Service(r"C:\Users\fizzy\Downloads\geckodriver-v0.36.0-win64\geckodriver.exe")
-
 driver = webdriver.Firefox(service=service, options=options)
-
 driver.get("https://libbyapp.com/search/nh")
+
+
+def check_availbility(bs):
+    status = bs.find('div', class_='title-status')
+    if status:
+        stat = status.get('title')
+        if stat and "Available to borrow" in stat:
+            return True
+    return False
+
+def get_title(bs):
+    title = bs.find('span', {'class' : 'title-tile-title'})
+    return title
 
 try:
 
     #searhing for the book - selenium
     search_input = WebDriverWait(driver, 15). until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[type=search]")))
     search_input.click()
-    search_input.send_keys("Little Women")
+    search_input.send_keys("Ghost Roast")
     search_input.send_keys(Keys.RETURN)
     curl = driver.current_url
     
@@ -32,8 +43,13 @@ try:
     html = driver.page_source
     bs = BeautifulSoup(html, 'lxml')
 
-    title = bs.find('span', {'class' : 'title-tile-title'})
-    print(title.text if title else "Title not found")
+    #title of first book
+    titles = get_title(bs)
+    print("Title: ", titles.text)
+
+    #avalilbility
+    availbe = check_availbility(bs)
+    print("Avablie:", availbe)
 
 except Exception as e:
     print("An error occures", e)
